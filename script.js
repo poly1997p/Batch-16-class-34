f07bc633a0991fd6420626fe114502f11777f76b
poly1997p <poly20221997@gmail.com>
2025-02-11 12:52:25 +0600

    relational operator

---
+++ script.js
@@ -0,0 +1,152 @@
+//relation operator
+
+//practice relation operator (>) 
+
+let a= 20;
+let b= 40;
+
+if(a>b){
+    console.log("b this is grater than a");
+}
+
+else{
+    console.log("b is not grater than a");
+}
+
+
+let c= 50;
+let d= 55;
+
+if(c<d){
+    console.log("c is lower than d");
+}
+
+else{
+    console.log("c is grater than d");
+}
+
+//practice relation operator (>=)
+
+let age=17;
+
+if(age>=18){
+    console.log("you are eligible to vote");
+}
+
+else{ 
+    console.log("you are not eligible to vote");
+}
+
+let year= 20;
+
+if(year<=30){
+    console.log("you are not eligible to admission");
+}
+
+else{
+  console.log("you are eligible to admission");
+}
+
+//maximum number (>)
+
+let num1= 34;
+let num2= 50;
+
+if(num1<num2){
+    console.log("num2 is maximum");
+}
+
+else{
+    console.log("num2 is not maximum");
+}
+
+let numbx= 60;
+let numby= 40;
+
+if(numbx>numby){
+    console.log("numbx is maximum");
+}
+
+else{
+    console.log("numbx is minimum");
+}
+
+//positive number(+)
+let number=89;
+
+if(number>0){
+    console.log("this is positive number");
+}
+
+else{
+    console.log("this is negative number");
+}
+
+//negative number(-)
+
+let numb= -2;
+
+if(numb<0){
+    console.log("this is negative number");
+}
+else{
+    console.log("this is negative number");
+}
+
+//even or odd checker
+
+let m= 75;
+
+if(m % 2==0){
+    console.log("this is even number");
+}
+
+else{
+    console.log("this is odd number");
+}
+
+let n=8;
+
+if(n%2==0){
+    console.log("this is even number");
+}
+
+else{
+    console.log("this is odd number");
+}
+
+//grade checker
+
+let marks=75;
+
+if(marks>=90){
+    console.log("the grade is A+");
+}
+
+else if(marks>=80){
+    console.log("the grade is A");
+}
+
+else if(marks>=70){
+    console.log("the grade is B");
+}
+else if(marks>=60){
+    console.log("the grade is c");
+}
+else if(marks>=50){
+    console.log("the grade is D");
+}
+else{
+    console.log("the grade is F");
+}
+
+//a number is divisible by 5
+
+let p= 30;
+
+if(p%5==0){
+    console.log("the number is divisible by 5");
+}
+else{
+    console.log("the number is not divisible by 5");
+}
