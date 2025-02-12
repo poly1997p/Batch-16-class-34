f8c2e852d16d0f917c19e1657573ed807c6d0614
poly1997p <poly20221997@gmail.com>
2025-02-12 12:32:48 +0600

    relational operator

--- script.js
+++ script.js
@@ -119,21 +119,24 @@ else{
 
 let marks=75;
 
-if(marks>=90){
+if(marks>=80){
     console.log("the grade is A+");
 }
 
-else if(marks>=80){
+else if(marks>=70){
     console.log("the grade is A");
 }
 
-else if(marks>=70){
-    console.log("the grade is B");
-}
 else if(marks>=60){
-    console.log("the grade is c");
+    console.log("the grade is A-");
 }
 else if(marks>=50){
+    console.log("the grade is B");
+}
+else if(marks>=40){
+    console.log("the grade is C");
+}
+else if(marks>=33){
     console.log("the grade is D");
 }
 else{
